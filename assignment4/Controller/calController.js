640f0843848f9eac077cb5a49de6cbb54a793b7b
Taofeek Olusola <olusolataofeek@gmail.com>
2024-10-02 18:09:51 +0100

    techyjaunt fourth assignment

--- assignment4/Controller/calController.js
+++ assignment4/Controller/calController.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 // @desc add two numbers
 // @route POST /api/calculate/add
-//access public
+// @access public
 const add = async (req, res) => {
     try {
         const {a, b} = req.body
@@ -23,7 +23,7 @@ const add = async (req, res) => {
 
 // @desc subtract two numbers
 // @route POST /api/calculate/subtract
-//access public
+// @access public
 const subtract = async (req, res) => {
     try {
         const {a, b} = req.body
@@ -49,4 +49,53 @@ const subtract = async (req, res) => {
     }
 }
 
-module.exports = {add, subtract}
+// @desc multiplies two numbers
+// @route POST /api/calculate/multiply
+// @access public
+const multiply = async (req, res) => {
+    try {
+        const {a, b} = req.body
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            res.status(404).json('a and b must be a number')
+            return
+        }
+
+        if (isNaN(a) || isNaN(b)) {
+            res.status(404).json('a and b must be valid numbers')
+        }
+        const c = await parseInt(a * b)
+        res.status(200).json(c)
+    } catch (error) {
+        res.status(500).json('message', error.message)
+    }
+}
+
+// @desc divides two numbers
+// @route POST /api/calculate/divide
+// @access public
+const divide = async (req, res) => {
+    try {
+        const {a, b} = req.body
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            res.status(404).json('a and b must be a number')
+            return
+        }
+
+        if (isNaN(a) || isNaN(b)) {
+            res.status(404).json('a and b must be valid numbers')
+            return
+        }
+
+        if (b <= 0) {
+            res.status(404).json('Can not devide by zero')
+            return
+        }
+
+        const c = await parseInt(a / b)
+        res.status(200).json(c)
+    } catch (error) {
+        res.status(500).json('message', error.message)
+    }
+}
+
+module.exports = {add, subtract, multiply, divide}
--- assignment4/router/calc.js
+++ assignment4/router/calc.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {add, subtract} = require('../Controller/calController');
+const {add, subtract, multiply, divide} = require('../Controller/calController');
 
 router.post('/add', add)
 router.post('/subtract', subtract)
+router.post('/multiply', multiply)
+router.post('/divide', divide)
 
 module.exports = router;
